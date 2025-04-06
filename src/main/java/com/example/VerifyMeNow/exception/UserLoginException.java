cde67ff80555626c85c5cf720f57566ec4079859
lucas <lucas.oklp@gmail.com>
2025-04-06 18:35:28 -0300

    remove fronend , renomeia de VerifyMeNow para backend

--- src/main/java/com/example/VerifyMeNow/exception/UserLoginException.java
+++
@@ -1,7 +0,0 @@
-package com.example.VerifyMeNow.exception;
-
-public class UserLoginException extends RuntimeException {
-    public UserLoginException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/VerifyMeNow/exception/UserRegistrationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.VerifyMeNow.exception;
-
-public class UserRegistrationException extends RuntimeException {
-    public UserRegistrationException(String message) {
-        super(message);
-    }
-}
